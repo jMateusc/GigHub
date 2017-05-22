a6d04cd0dbe0494a472dc87054b8ac09b34d22c4
Joao Mateus <jmateuscunha@gmail.com>
2017-05-22 20:51:33 -0300

    -AddGenreNGigs

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; } //Artistas
+        public DateTime DateTime { get; set; }      //Data
+        public string Venue { get; set; }           //Local
+        public Genre Genre { get; set; }            //Associa
+
+
+    }
+}
