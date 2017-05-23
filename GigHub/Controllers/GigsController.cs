fb4b4c784ca6fd497a540d95dbce2206ab914fc7
Joao Mateus <jmateuscunha@gmail.com>
2017-05-23 12:34:06 -0300

    -RefatorarDateTime do CreateController para GigViewModel

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,6 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -48,7 +47,7 @@ namespace GigHub.Controllers
             {
                 //resgata usuario.id logado
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                DateTime = viewModel.DateTime,
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
