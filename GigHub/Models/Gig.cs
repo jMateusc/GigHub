825a674f897f2508c2d185bf5b1f82d254e6c5ed
Joao Mateus <jmateuscunha@gmail.com>
2017-05-22 21:01:27 -0300

    -AdicionadoConstraintsGigNGenre

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
     public class Gig
     {
         public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Artist { get; set; } //Artistas
+
         public DateTime DateTime { get; set; }      //Data
+
+        [Required]
+        [StringLength(255)]
         public string Venue { get; set; }           //Local
+
+        [Required]
         public Genre Genre { get; set; }            //Associa
 
 
