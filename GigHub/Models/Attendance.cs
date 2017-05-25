ee6fac278b68d3c56f4f817d3b7da03598913f7f
Joao Mateus <jmateuscunha@gmail.com>
2017-05-25 10:41:25 -0300

    -AddAttendanceTable & boostrap design(index page)

---
+++ GigHub/Models/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class Attendance
+    {
+        //Tabela de Composição User <-> Gig
+        //Comparecimento
+
+        public Gig Gig { get; set; }
+        public ApplicationUser Attendee { get; set; }
+
+
+        //(FK)
+        [Key]
+        [Column(Order = 1)]
+        public int GigId { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public string AttendeeId { get; set; }
+    }
+}
--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -31,10 +31,17 @@ namespace GigHub.Models
 
         //Relaciona Tabelas Criadas ao Contexto
         public DbSet<Gig> Gigs { get; set; }
-        public DbSet<Genre> Genres { get; set; } 
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
-
-
+        //*************************
+        //FluentAPI OnDeleteCascade Alterações
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Cada Comparecimento possui uma Gig associada  Many-to-Many
+            modelBuilder.Entity<Attendance>().HasRequired(a => a.Gig).WithMany().WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
+        }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
