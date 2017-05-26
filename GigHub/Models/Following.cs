2a7763da024378a5387217b6182e2fbf29718eda
Joao Mateus <jmateuscunha@gmail.com>
2017-05-25 23:13:05 -0300

    -AddFollowing Model/DbSet<>/ApplicationUser

---
+++ GigHub/Models/Following.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class Following
+    {
+        /*Lembrando que esta é uma tabela de composição onde
+         *apenas relaciona 2 tabelas e associa suas id<->id
+         *por isso o nome de tabela de composição
+         */
+
+        [Key]
+        [Column(Order = 1)]
+        public string FollowerId { get; set; }   //Seguidor
+        [Key]
+        [Column(Order = 2)]
+        public string FolloweeId { get; set; }   //Pessoa que é seguida por outra
+
+
+        //Associa
+        public ApplicationUser Follower { get; set; }
+        public ApplicationUser Followee { get; set; }
+    }
+}
--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
@@ -16,6 +18,15 @@ namespace GigHub.Models
         public string Name { get; set; }
 
 
+        //A SER ENTENDIDO
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -33,6 +44,7 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
 
         //*************************
         //FluentAPI OnDeleteCascade Alterações
@@ -40,6 +52,10 @@ namespace GigHub.Models
         {
             //Cada Comparecimento possui uma Gig associada  Many-to-Many
             modelBuilder.Entity<Attendance>().HasRequired(a => a.Gig).WithMany().WillCascadeOnDelete(false);
+
+            //A ENTENDER (um usuario pode possuir varios seguidores) && (um usuario pode seguir varias pessoas)
+            modelBuilder.Entity<ApplicationUser>().HasMany(u => u.Followers).WithRequired(f => f.Followee).WillCascadeOnDelete(false);
+            modelBuilder.Entity<ApplicationUser>().HasMany(u => u.Followees).WithRequired(f => f.Follower).WillCascadeOnDelete(false);
             base.OnModelCreating(modelBuilder);
         }
 
