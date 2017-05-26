8894264cd6a830eab7057cfa1836e5b03614700a
Joao Mateus <jmateuscunha@gmail.com>
2017-05-25 23:39:40 -0300

    -AddFollowingsController/Dto/Ajax (v.1.0)

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            //Usuario logado é quem vai seguir determinado evento/artista
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId);
+            if (exists)
+            {
+                return BadRequest("Already following this event/artist.");
+            }
+
+            //associa
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
